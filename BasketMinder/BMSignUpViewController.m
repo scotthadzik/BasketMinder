280fe0d1a7f7af36fd478918241805d849669e34
Scott Hadzik <scotthadzik@weber.edu>
2013-12-21 12:54:06 -0700

    -attempt at user validation

--- BasketMinder/BMSignUpViewController.m
+++ BasketMinder/BMSignUpViewController.m
@@ -28,10 +28,46 @@
 
 - (IBAction)signUp:(id)sender {
     
-    NSString *email = [self.emailField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]; //convert the textfield into a string and trim white space
-    email = [email lowercaseString];
-    NSString *password = [self.passwordField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]; //convert the textfield into a string
+   // NSString *email = [self.emailField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]; //convert the textfield into a string and trim white space
+   // email = [email lowercaseString];
+    //NSString *password = [self.passwordField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]; //convert the textfield into a string
+    NSString *email = @"sgthad@gmail.com";
+    NSString *password = @"password";
     
+    //login validation
+    
+       // NSString *post = [NSString stringWithFormat:@"?email=%@&password=%@",email,password];
+      //  NSString *post = @"email=sgthad@gmail.com&password=password";//,[self urlEncodeValue:email], [self urlEncodeValue:password];
+        NSString *post = [NSString stringWithFormat:@"email=%@&password=%@", [self urlEncodeValue:email], [self urlEncodeValue:password]];
+        // NSLog(@"email: %@",email);
+       // NSLog(@"password: %@",password);
+        NSLog(@"Post: %@",post);
+        NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+        NSString *postLength = [NSString stringWithFormat:@"%d",[postData length]];
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+        [request setURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://contributions1.bountifulbaskets.org"]]];
+        [request setHTTPMethod:@"POST"];
+        [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+        [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Current-Type"];
+        NSLog(@"Post Data: %@",postData);
+        [request setHTTPBody:postData];
+    
+    
+    
+    //+ (void)sendAsynchronousRequest:(NSURLRequest )request queue:(NSOperationQueue *)queue completionHandler:(void (^)(NSURLResponse, //NSData*, NSError*))handler –
+    
+    
+    
+        NSHTTPURLResponse* urlResponse = nil;
+        NSError *error = [[NSError alloc] init];
+        NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&error];
+        NSString *result = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+        NSLog(@"Response Code: %d", [urlResponse statusCode]);
+    
+       if ([urlResponse statusCode] >= 200 && [urlResponse statusCode] < 300)
+        {
+            NSLog(@"Response: %@", result);
+        }
     
     if([email length] == 0 || [password length] == 0){
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Oops" message:@"Make sure you enter an E-mail and Password" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
@@ -58,6 +94,13 @@
         
     }
 
+}
+
+- (NSString *)urlEncodeValue:(NSString *)str
+{
+    NSString *result = (NSString *) CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)str, NULL, CFSTR("?=&+"), kCFStringEncodingUTF8));
+    NSLog(@"result : %@", result);
+    return result;
 }
 
 @end
--- BasketMinder/BMloginViewController.m
+++ BasketMinder/BMloginViewController.m
@@ -10,7 +10,7 @@
 #import "BMWebsiteViewController.h"
 #import <Parse/Parse.h>
 
-@interface BMloginViewController () 
+@interface BMloginViewController () <UITextFieldDelegate>
 
 @end
 
@@ -22,9 +22,9 @@
     self.navigationItem.hidesBackButton = YES;
     
     
-     PFUser *currentUser = [PFUser currentUser];
-    _emailField.text = currentUser.email;
-    _passwordField.text = currentUser.password;
+ //    PFUser *currentUser = [PFUser currentUser];
+ //   _emailField.text = currentUser.email;
+ //  _passwordField.text = currentUser.password;
 }
 
 //dismiss the keyboard when touched outside of text field
@@ -39,34 +39,6 @@
     NSString *email = [self.emailField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]; //convert the textfield into a string and trim white space
     email = [email lowercaseString];
     NSString *password = [self.passwordField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]; //convert the textfield into a string
-    
-    //login validation
-    
-//   // NSString *post = [NSString stringWithFormat:@"&UserName=%@&Password=%@",email,password];
-//    NSString *post = [NSString stringWithFormat:@"&UserName=%@&Password=%@",email,password];
-//    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-//    NSString *postLength = [NSString stringWithFormat:@"%d",[postData length]];
-//    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-//    [request setURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://contributions4.bountifulbaskets.org"]]];
-//    [request setHTTPMethod:@"POST"];
-//    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-//    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Current-Type"];
-//    [request setHTTPBody:postData];
-//    
-//    NSHTTPURLResponse* urlResponse = nil;
-//    NSError *error = [[NSError alloc] init];
-//    NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&error];
-//    NSString *result = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
-//    NSLog(@"Response Code: %d", [urlResponse statusCode]);
-//    
-//    if ([urlResponse statusCode] >= 200 && [urlResponse statusCode] < 300)
-//    {
-//        NSLog(@"Response: %@", result);
-//    }
-    
-    
-    
-    
     
     if([email length] == 0 || [password length] == 0){
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Oops" message:@"Make sure you enter an E-mail and Password" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
